4c304a9522950c8b5b117299593af894a8dce613
Andrey Knyshenko <andrei.knyshenko@gmail.com>
2020-04-19 22:20:13 +0400

    fix: fix header,menu components render

--- english-for-kids/src/views/header.js
+++ english-for-kids/src/views/header.js
@@ -1,14 +1,17 @@
 /* global document */
 import $header from './templates/header';
-import headerListeners from './listeners/headerListeners';
+import $headerListeners from './listeners/headerListeners';
 
 
 export default class Header {
   constructor(_mode) {
     this.mode = _mode;
     this.headerTemplate = $header;
-    this.headerListeners = headerListeners;
+    this.headerListeners = $headerListeners;
     this.selfObj = {};
+    this.headerWrapper = {};
+    this.modeSwitcher = {};
+    this.menuButton = {};
     this.init();
   }
 
@@ -21,14 +24,20 @@ export default class Header {
 
   setMode(_mode) {
     if (this.mode !== _mode) {
+      this.headerWrapper.classList.replace(this.mode, _mode);
       this.mode = _mode;
-      document.getElementById('header_wrapper').classList[1] = this.mode;
-      document.getElementById('mode_switcher').checked = (this.mode !== 'train');
+      this.modeSwitcher.checked = (this.mode !== 'train');
     }
   }
 
-  async render(_mode, _currPage) {
-    this.selfObj.innerHtml = await $header(_mode, _currPage);
-    await this.headerListeners();
+  render(_mode, _currPage) {
+    this.selfObj.innerHTML = $header(_mode, _currPage);
+
+    this.headerWrapper = document.getElementById('header_wrapper');
+    this.modeSwitcher = document.getElementById('mode_switcher');
+    this.menuButton = document.getElementById('menu__toggle');
+
+    this.headerListeners.onModeSwitch(this.modeSwitcher);
+    this.headerListeners.onMenuButton(this.menuButton);
   }
 }
--- english-for-kids/src/views/menu.js
+++ english-for-kids/src/views/menu.js
@@ -1,39 +1,44 @@
 /* global document */
-import $menu from './templates/menu';
 
+import $menu from './templates/menu';
+import $menuListeners from './listeners/menuListeners';
 
 export default class Menu {
   constructor(_mode, _links) {
     this.mode = _mode;
     this.links = _links;
     this.menuTemplate = $menu;
+    this.menuListeners = $menuListeners;
     this.selfObj = {};
+    this.show = 'hide';
+    this.menuBody = {};
     this.init();
   }
 
   init() {
     this.selfObj = document.createElement('nav');
     this.selfObj.id = 'nav';
+    this.selfObj.classList = 'hide';
     document.getElementById('header').after(this.selfObj);
     this.render(this.mode, this.links);
   }
 
   setMode(_mode) {
     if (this.mode !== _mode) {
+      this.menuBody.classList.replace(this.mode, _mode);
       this.mode = _mode;
-      document.getElementById('menu_wrapper').classList[1] = this.mode;
     }
   }
 
-  menuShow(_show) {
-    if (_show === true) {
-      this.selfObj.classList[1] = 'show';
-    } else {
-      this.selfObj.classList[1] = 'hide';
+  showMenu(_isShow) {
+    if (this.show !== _isShow) {
+      this.selfObj.classList.replace(this.show, _isShow);
+      this.show = _isShow;
     }
   }
 
   render(_mode, _links) {
-    this.selfObj.innerHtml = $menu(_mode, _links);
+    this.selfObj.innerHTML = $menu(_mode, _links);
+    this.menuBody = document.getElementById('menu_body');
   }
 }
