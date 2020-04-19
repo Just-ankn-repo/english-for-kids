30db981bfd5d8f77e2062df9454c1fd10a32cb4b
Andrey Knyshenko <andrei.knyshenko@gmail.com>
2020-04-19 22:22:53 +0400

    fix: fix routing

--- english-for-kids/src/router.js
+++ english-for-kids/src/router.js
@@ -3,15 +3,17 @@
 import Controller from './controller';
 import $setListener from './utils/setListener';
 import $route from './utils/route';
+import $handler from './handler';
 
 
 export default {
   init() {
-    const controller = new Controller();
-    if (document.location.hash.match(/^#categories\/&|^#category\//) === null) {
+    if (document.location.hash.match(/^#categories\/&|^#category\/|^#statistics\//) === null) {
       document.location.hash = '#categories/';
     }
-    $setListener(window, 'load', $route(controller));
-    $setListener(window, 'hashchange', $route(controller));
+    const controller = new Controller();
+    $handler.controller = controller;
+    $setListener(window, 'load', () => $route(controller));
+    $setListener(window, 'hashchange', () => $route(controller));
   },
 };
--- english-for-kids/src/utils/route.js
+++ english-for-kids/src/utils/route.js
@@ -1,6 +1,6 @@
 /* global document */
 
-export default function route(_controller) {
+export default (_controller) => {
   const location = document.location.hash.slice(1);
   const [page, category] = location.split('/');
   _controller.setPage([page, category]);
