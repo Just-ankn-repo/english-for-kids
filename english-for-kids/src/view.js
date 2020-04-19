383b277e9826cff9347e75625639a6170cba9ee6
Andrey Knyshenko <andrei.knyshenko@gmail.com>
2020-04-19 22:23:49 +0400

    feat: add game mode methods in view

--- english-for-kids/src/view.js
+++ english-for-kids/src/view.js
@@ -5,7 +5,6 @@ import Header from './views/header';
 import Menu from './views/menu';
 import Categories from './views/categories';
 import Category from './views/category';
-import Statistics from './views/statistics';
 
 export default class View {
   constructor(_mode, _links) {
@@ -15,24 +14,37 @@ export default class View {
     this.menu = new Menu(_mode, _links);
     this.categories = new Categories();
     this.category = new Category();
-    this.statistics = new Statistics();
     this.currPage = [];
   }
 
   setMode(_mode) {
     if (this.mode !== _mode) {
+      this.mode = _mode;
       this.header.setMode(this.mode);
-      this.menu.setMode(this.mode, this.links);
-      this.render(this.currPage);
+      this.menu.setMode(this.mode);
+      this[this.currPage[0]].setMode(this.mode);
     }
   }
 
-  showMenu(_change) {
-    this.menu.menuShow(_change);
+  showMenu(_isShow) {
+    this.menu.showMenu(_isShow);
   }
 
   render(_toRender, _data) {
+    this.currPage = _toRender;
     this.header.render(this.mode, this.currPage);
     this[_toRender[0]].render(this.mode, _data || null, _toRender[1]);
+  }
+
+  gameAnswer(_word, _answer) {
+    this.category.gameAnswer(_word, _answer);
+  }
+
+  playWord(_word) {
+    this.category.playWord(_word);
+  }
+
+  renderEndGame(_answers) {
+    this.category.renderEndGame(_answers);
   }
 }
